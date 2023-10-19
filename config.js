dbe69d40e4d8cca6010163f55db28d5b9b28dc31
QuanLe <anhquan28323@gmail.com>
2023-10-19 23:08:24 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng myiudoi';
+const contentCard = 'Chúc em iu của anh 20/10 tràn ngập niềm vui , hạnh phúc và những nụ cười. Chúc cho những điều tốt đẹp sẽ đến với chúng ta. Anh yêu em nhiều lắm hihi';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hoa.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
